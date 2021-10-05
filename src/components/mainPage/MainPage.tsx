4b1eed2f98618614b7254e820ea54940fb427358
강은빈 <kub0803@naver.com>
2021-10-05 22:50:34 +0900

    [ADD] 메인 컴포넌트 추가

---
+++ src/components/mainPage/MainPage.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Header } from "..";
+import * as S from "./style";
+
+const MainPage = () => {
+  return (
+    <S.MainWrapper>
+      <Header />
+    </S.MainWrapper>
+  );
+};
+
+export default MainPage;
