3ac4a50ffd2cc64be032ae169b6591b9cbf2d0dc
강은빈 <kub0803@naver.com>
2021-10-05 22:50:17 +0900

    [ADD] Header 컴포넌트 추가

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from './style'
+
+const Header = () => {
+    return (
+        <S.HeaderWrapper>
+            
+        </S.HeaderWrapper>
+    )
+};
+
+export default Header;
