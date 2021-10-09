2397b70d77dc4d89115801b05547f62631b956c0
강은빈 <kub0803@naver.com>
2021-10-10 00:05:28 +0900

    [MODIFY] 헤더 스타일 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,17 @@
 import React from "react";
-import * as S from './style'
+import { Link } from "react-router-dom";
+import { Logo } from "../../utils/assets";
+import * as S from "./style";
 
 const Header = () => {
-    return (
-        <S.HeaderWrapper>
-            
-        </S.HeaderWrapper>
-    )
+  return (
+    <S.HeaderWrapper>
+      <Link to="/">
+        <img src={Logo} alt="pick로고" title="pick로고" />
+      </Link>
+      <span>로그아웃</span>
+    </S.HeaderWrapper>
+  );
 };
 
 export default Header;
--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,7 +1,28 @@
 import styled from "@emotion/styled";
+import { HeadFootColor, MainColor } from "../../utils/color/color";
 
 export const HeaderWrapper = styled.div`
   width: 100%;
-  height: 100px;
-  background-color: #e5e5e5;
+  height: 80px;
+  background-color: ${HeadFootColor};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  a > img {
+    width: 200px;
+  }
+
+  span {
+    position: absolute;
+    right: 0;
+    margin: 0 30px;
+    font-size: 22px;
+    color: ${MainColor};
+  }
+`;
+
+export const FooterWrapper = styled(HeaderWrapper)`
+  position: absolute;
+  bottom: 0;
 `;
