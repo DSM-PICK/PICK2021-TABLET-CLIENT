08ac60ee8112de3835d1c7ef09d03fcc018b4333
강은빈 <kub0803@naver.com>
2021-10-10 00:06:25 +0900

    [ADD] 타이틀 컴포넌트 추가

---
+++ src/components/mainPage/mainItem/Title.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import * as S from "./style";
+
+const Title = () => {
+  return (
+    <S.Title>
+      <span>강은빈 선생님은</span>
+      <div className="title-item">
+        <span>전공동아리</span>
+        <span>자습감독이십니다.</span>
+      </div>
+    </S.Title>
+  );
+};
+
+export default Title;
