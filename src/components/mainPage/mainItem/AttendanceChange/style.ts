f9f0ccd39881e28eec0e5c9fbfed35c340dd5e3f
강은빈 <kub0803@naver.com>
2021-10-12 23:48:04 +0900

    [ADD] 출석 선택 컴포넌트 추가

---
+++ src/components/mainPage/mainItem/AttendanceChange/style.ts
@@ -0,0 +1,21 @@
+import styled from "@emotion/styled";
+
+export const AttendanceChangeWrapper = styled.div`
+  margin: 40px 0;
+  width: 100%;
+  height: 28%;
+
+  h1 {
+    font-size: 24px;
+  }
+`;
+
+export const ChangeBox = styled.div`
+  margin-top: 20px;
+  width: 100%;
+  height: 80%;
+  background: #fbfbfb;
+  border: 1px solid #efefef;
+  box-sizing: border-box;
+  box-shadow: 0px 3px 6px rgba(210, 210, 210, 0.25);
+`;
