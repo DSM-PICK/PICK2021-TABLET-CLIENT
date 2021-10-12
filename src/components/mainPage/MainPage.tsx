f068803b84b25600145c433f18a7e4ad75649d04
강은빈 <kub0803@naver.com>
2021-10-12 23:48:38 +0900

    [MODIFY] 메인페이지 UI 변경

--- src/components/mainPage/MainPage.tsx
+++ src/components/mainPage/MainPage.tsx
@@ -1,11 +1,18 @@
-import React from "react";
-import { Header } from "..";
+import React, { useEffect, useRef, useState } from "react";
+import { AttendanceChange, AttendanceSelect, Footer, Header, Title } from "..";
 import * as S from "./style";
 
 const MainPage = () => {
   return (
     <S.MainWrapper>
       <Header />
+      <S.Container>
+        <Title />
+        <AttendanceSelect />
+        <AttendanceChange />
+        <AttendanceChange />
+      </S.Container>
+      <Footer />
     </S.MainWrapper>
   );
 };
--- src/components/mainPage/style.ts
+++ src/components/mainPage/style.ts
@@ -1,3 +1,20 @@
 import styled from "@emotion/styled";
 
-export const MainWrapper = styled.div``;
+export const MainWrapper = styled.div`
+  width: 100%;
+  height: 100vh;
+`;
+
+export const Container = styled.div`
+  width: 100%;
+  height: 83%;
+  margin: 0 auto;
+  overflow: auto;
+  padding: 0 7%;
+  box-sizing: border-box;
+
+  canvas {
+    position: absolute;
+    border: 1px solid;
+  }
+`;
