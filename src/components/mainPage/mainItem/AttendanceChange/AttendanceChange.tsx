112237573f66f3a117d44233667116811f0ef92b
강은빈 <kub0803@naver.com>
2021-10-12 23:48:23 +0900

    [ADD] 출석 컴포넌트 추가

---
+++ src/components/mainPage/mainItem/AttendanceChange/AttendanceChange.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import * as S from "./style";
+
+const AttendanceChange = () => {
+  return (
+    <S.AttendanceChangeWrapper>
+      <h1>출결 변동 사항 등록</h1>
+      <S.ChangeBox>
+          
+      </S.ChangeBox>
+    </S.AttendanceChangeWrapper>
+  );
+};
+
+export default AttendanceChange;
---
+++ src/components/mainPage/mainItem/AttendanceChange/ChangeList.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import * as S from './style'
+
+const ChangeList = () => {
+    
+}
