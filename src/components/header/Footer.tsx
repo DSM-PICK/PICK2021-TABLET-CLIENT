2657007234eaaf748b67351b209b5d1dd2ddf972
강은빈 <kub0803@naver.com>
2021-10-12 23:48:52 +0900

    [MODIFY] Footer 수정

--- src/components/header/Footer.tsx
+++ src/components/header/Footer.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import * as S from "./style";
 
-const Footer = () => {};
+const Footer = () => {
+  return <S.FooterWrapper></S.FooterWrapper>;
+};
 
 export default Footer;
