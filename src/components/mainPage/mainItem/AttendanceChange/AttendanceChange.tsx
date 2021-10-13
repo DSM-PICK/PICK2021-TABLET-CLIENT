6313c6e8101969065d7be59da4ad5f0ba76439c9
강은빈 <kub0803@naver.com>
2021-10-13 20:52:26 +0900

    [MODIFY] 출결 변동 UI 수정

--- src/components/mainPage/mainItem/AttendanceChange/AttendanceChange.tsx
+++ src/components/mainPage/mainItem/AttendanceChange/AttendanceChange.tsx
@@ -1,12 +1,83 @@
-import React from "react";
+import React, { forwardRef, useEffect, useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { Calendar } from "../../../../utils/assets";
+import { FieldButtonType } from "../../../../utils/interface/Attendance/FieldButton";
 import * as S from "./style";
 
+const FieldButton: FieldButtonType[] = [
+  {
+    id: 1,
+    field: "외출",
+  },
+  {
+    id: 2,
+    field: "현체",
+  },
+  {
+    id: 3,
+    field: "귀가",
+  },
+  {
+    id: 4,
+    field: "이동",
+  },
+  {
+    id: 5,
+    field: "취업",
+  },
+];
+
 const AttendanceChange = () => {
+  const [fieldSelect, setFieldSelect] = useState<number>(1);
+
+  const [startDate, setStartDate] = useState<any>(new Date());
+
+  const ExampleCustomInput = forwardRef(({ value, onClick }: any, ref: any) => (
+    <button className="example-custom-input" onClick={onClick} ref={ref}>
+      {value}
+    </button>
+  ));
+
+  useEffect(() => {
+    console.log(startDate);
+  }, [startDate]);
+
   return (
     <S.AttendanceChangeWrapper>
       <h1>출결 변동 사항 등록</h1>
       <S.ChangeBox>
-          
+        <S.Enrollment>
+          <div className="enrollment-item">
+            <S.SubTitle>종류</S.SubTitle>
+            <div className="field-item">
+              {FieldButton.map((item) => (
+                <span>{item.field}</span>
+              ))}
+            </div>
+          </div>
+          <div className="enrollment-item">
+            <S.SubTitle>날짜</S.SubTitle>
+            <DatePicker
+              selected={startDate}
+              onChange={(date) => setStartDate(date)}
+              customInput={<ExampleCustomInput />}
+            />
+          </div>
+          <div className="enrollment-item">
+            <S.SubTitle>이름</S.SubTitle>
+            <div className="field-item">
+              {FieldButton.map((item) => (
+                <span>{item.field}</span>
+              ))}
+            </div>
+          </div>
+          <div className="enrollment-item">
+            <S.SubTitle>사유</S.SubTitle>
+            <input type="text" placeholder="사유를 입력해주세요" />
+          </div>
+        </S.Enrollment>
+        <S.EnrollmentHistory></S.EnrollmentHistory>
       </S.ChangeBox>
     </S.AttendanceChangeWrapper>
   );
--- src/components/mainPage/mainItem/AttendanceChange/style.ts
+++ src/components/mainPage/mainItem/AttendanceChange/style.ts
@@ -1,12 +1,16 @@
 import styled from "@emotion/styled";
+import { BoxColor, MainColor } from "../../../../utils/color/color";
 
 export const AttendanceChangeWrapper = styled.div`
   margin: 40px 0;
   width: 100%;
-  height: 28%;
+  height: 33%;
 
   h1 {
     font-size: 24px;
+    padding-bottom: 3px;
+    border-bottom: 3px solid ${MainColor};
+    width: fit-content;
   }
 `;
 
@@ -14,8 +18,46 @@ export const ChangeBox = styled.div`
   margin-top: 20px;
   width: 100%;
   height: 80%;
-  background: #fbfbfb;
+  background: ${BoxColor};
   border: 1px solid #efefef;
   box-sizing: border-box;
   box-shadow: 0px 3px 6px rgba(210, 210, 210, 0.25);
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-evenly;
+`;
+
+export const Enrollment = styled.div`
+  width: 55%;
+  height: 70%;
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .enrollment-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    .field-item {
+      display: flex;
+      span {
+        padding: 9px 15px;
+      }
+    }
+  }
+`;
+
+export const EnrollmentHistory = styled.div`
+  width: 35%;
+  height: 70%;
+  border: 1px solid red;
+`;
+
+export const SubTitle = styled.span`
+  font-size: 18px;
+  font-weight: 600;
+  padding: 9px 0;
 `;
