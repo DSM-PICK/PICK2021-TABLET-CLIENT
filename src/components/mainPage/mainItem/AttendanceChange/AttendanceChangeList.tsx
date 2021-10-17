40fba5f01150aedb1c9b270f4ba0ceb3806a4e1a
강은빈 <kub0803@naver.com>
2021-10-17 23:39:27 +0900

    [ADD] 출결 기록 리스트 컴포넌트

---
+++ src/components/mainPage/mainItem/AttendanceChange/AttendanceChangeList.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import * as S from "./style";
+
+const AttendanceChangeList = () => {
+  return <S.AttendanceChangeWrapper></S.AttendanceChangeWrapper>;
+};
+
+export default AttendanceChangeList;
