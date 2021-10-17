77df8f0a0b75b2088cead89f292f9493081fd901
강은빈 <kub0803@naver.com>
2021-10-17 23:38:03 +0900

    [ADD] 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,3 +4,5 @@ export { default as MainPage } from "./mainPage/MainPage";
 export { default as Title } from "./mainPage/mainItem/Title";
 export { default as AttendanceSelect } from "./mainPage/mainItem/AttendanceSelect";
 export { default as AttendanceChange } from "./mainPage/mainItem/AttendanceChange/AttendanceChange";
+export { default as EnrollmentHistory } from "./mainPage/mainItem/AttendanceChange/EnrollmentHistory";
+export { default as AttendanceChangeList } from "./mainPage/mainItem/AttendanceChange/AttendanceChangeList";
