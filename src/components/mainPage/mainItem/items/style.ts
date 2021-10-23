054d4d13a8d67c298f90953a6432b6aa35108b80
강은빈 <kub0803@naver.com>
2021-10-24 02:06:58 +0900

    [MODIFY] 파일 이름 수정

--- src/components/mainPage/mainItem/items/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-import { MainColor } from "../../../../utils/color/color";
-
-export const Title = styled.h1`
-  font-size: 24px;
-  padding-bottom: 5px;
-  border-bottom: 3px solid ${MainColor};
-  width: fit-content;
-`;
