63f5e95216c965ee74af5e08ddaf2f6834d23a2f
강은빈 <kub0803@naver.com>
2021-10-24 02:09:56 +0900

    [FEAT] 다중 선택 금지

--- src/components/mainPage/mainItem/AttendanceChangeList/AttendanceChangeList.tsx
+++ src/components/mainPage/mainItem/AttendanceChangeList/AttendanceChangeList.tsx
@@ -1,8 +1,36 @@
-import React from "react";
-import * as S from "../AttendanceChange/style";
+import React, { useState } from "react";
+import * as S from "./style";
+import { SubTitle } from "../../../index";
+import {
+  ClassButtonList,
+  FieldButtonType,
+} from "../../../../utils/interface/Attendance/FieldButtonType";
+import FloorClassItem from "./FloorClassItem";
+import ListContainer from "./ListContainer";
 
 const AttendanceChangeList = () => {
-  return <S.AttendanceChangeWrapper></S.AttendanceChangeWrapper>;
+  const [selected, setSelected] = useState<number>(1);
+
+  const selectedHandlerColor = (item: FieldButtonType) => {
+    setSelected(item.id);
+  };
+
+  return (
+    <S.MainWrapper>
+      <SubTitle title="출결변동내역" />
+      <S.SelectButtonWrapper>
+        {ClassButtonList.map((item) => (
+          <FloorClassItem
+            key={item.id}
+            item={item}
+            selectedHandlerColor={selectedHandlerColor}
+            selected={selected}
+          />
+        ))}
+      </S.SelectButtonWrapper>
+      <ListContainer />
+    </S.MainWrapper>
+  );
 };
 
 export default AttendanceChangeList;
