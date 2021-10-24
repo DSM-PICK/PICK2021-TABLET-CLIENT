2631f7f0a069dc93e303a93871ade186e82c8304
강은빈 <kub0803@naver.com>
2021-10-24 19:51:43 +0900

    [MODIFY] 헤더 UI 수정

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -1,6 +1,10 @@
 import styled from "@emotion/styled";
 import { HeadFootColor, MainColor } from "../../utils/color/color";
 
+interface Props {
+  modal: boolean;
+}
+
 export const HeaderWrapper = styled.div`
   width: 100%;
   height: 80px;
@@ -8,7 +12,7 @@ export const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  
+
   a > img {
     width: 200px;
   }
@@ -28,4 +32,27 @@ export const FooterWrapper = styled(HeaderWrapper)`
   display: flex;
   flex-direction: row;
   justify-content: space-around;
+
+  img {
+    width: 50px;
+  }
+`;
+
+export const ModalWrapper = styled.div<Props>`
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  right: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.44);
+  display: ${({ modal }) => (modal ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+
+  .box {
+    width: 400px;
+    height: 250px;
+    background-color: white;
+  }
 `;
