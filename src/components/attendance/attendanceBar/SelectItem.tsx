9f4a273a198705b6d25bf6c90fe0aa16d14551bd
강은빈 <kub0803@naver.com>
2021-10-24 20:38:17 +0900

    [ADD] 장소 선택 아이템 컴포넌트

---
+++ src/components/attendance/attendanceBar/SelectItem.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { MainColor } from "../../../utils/color/color";
+import { FieldButtonType } from "../../../utils/interface/Attendance/FieldButtonType";
+import * as S from "./style";
+
+interface Props {
+  item: FieldButtonType;
+  selected: number;
+  selectedHandlerColor: any;
+}
+
+const SelectItem = ({ item, selected, selectedHandlerColor }: Props) => {
+  return (
+    <S.SelectItem
+      onClick={() => selectedHandlerColor(item)}
+      style={
+        selected === item.id
+          ? { backgroundColor: `${MainColor}`, color: "white" }
+          : { backgroundColor: "", color: "black" }
+      }
+    >
+      {item.field}
+    </S.SelectItem>
+  );
+};
+
+export default SelectItem;
---
+++ src/components/attendance/attendanceBar/style.ts
@@ -0,0 +1,16 @@
+import styled from "@emotion/styled";
+
+export const SelectBarWrapper = styled.div`
+  height: 40px;
+  display: flex;
+  flex-direction: row;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.14); ;
+`;
+
+export const SelectItem = styled.div`
+  width: calc(100% / 5);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+`;
