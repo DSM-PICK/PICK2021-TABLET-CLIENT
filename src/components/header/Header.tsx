d09bfdef90489d14563774ab72598d056003158b
강은빈 <kub0803@naver.com>
2021-10-24 19:50:41 +0900

    [MODIFY] 헤더 수정

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,16 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Logo } from "../../utils/assets";
+import LogoutModal from "./LogoutModal";
 import * as S from "./style";
 
 const Header = () => {
+  const [modal, setModal] = useState<boolean>(false);
+
   return (
-    <S.HeaderWrapper>
-      <Link to="/">
-        <img src={Logo} alt="pick로고" title="pick로고" />
-      </Link>
-      <span>로그아웃</span>
-    </S.HeaderWrapper>
+    <>
+      <LogoutModal modal={modal} setModal={setModal} />
+      <S.HeaderWrapper>
+        <Link to="/">
+          <img src={Logo} alt="pick로고" title="pick로고" />
+        </Link>
+        <span onClick={() => setModal(!modal)}>강은빈 선생님</span>
+      </S.HeaderWrapper>
+    </>
   );
 };
 
