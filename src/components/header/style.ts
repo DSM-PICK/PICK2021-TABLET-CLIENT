a4cb37a2737482fec6a5d1bd882c2d6d0a7f3240
강은빈 <kub0803@naver.com>
2021-10-24 20:39:20 +0900

    [MODIFY] 헤더 스타일 변경

--- src/components/header/style.ts
+++ src/components/header/style.ts
@@ -12,6 +12,7 @@ export const HeaderWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.07);
 
   a > img {
     width: 200px;
