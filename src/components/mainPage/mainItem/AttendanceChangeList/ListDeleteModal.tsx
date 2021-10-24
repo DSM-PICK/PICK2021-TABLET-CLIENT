f39cf286b9991c2992e50c8f38cdf84b298de29a
강은빈 <kub0803@naver.com>
2021-10-24 19:51:08 +0900

    [MODIFY] 리스트 삭제 모달

--- src/components/mainPage/mainItem/AttendanceChangeList/ListDeleteModal.tsx
+++ src/components/mainPage/mainItem/AttendanceChangeList/ListDeleteModal.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Close, Logo } from "../../../../utils/assets";
 import * as S from "./style";
 
 interface Props {
@@ -9,7 +10,21 @@ interface Props {
 const ListDeleteModal = ({ modal, setModal }: Props) => {
   return (
     <S.ModalWrapper modal={modal}>
-      <div className="box"></div>
+      <div className="box">
+        <img src={Logo} alt="logo" id="logo" />
+        <img
+          src={Close}
+          alt="닫기 아이콘"
+          className="close-icon"
+          onClick={() => setModal(!modal)}
+        />
+        <span>출결 변동 사항을 삭제하시겠습니까?</span>
+
+        <div className="button-wrapper">
+          <button onClick={() => setModal(!modal)}>삭제</button>
+          <button onClick={() => setModal(!modal)}>취소</button>
+        </div>
+      </div>
     </S.ModalWrapper>
   );
 };
