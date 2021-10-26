a27b7156fb7a22e29231ec20a3a0e9ac3a45515d
강은빈 <kub0803@naver.com>
2021-10-26 23:39:18 +0900

    [ADD] props 추가

--- src/components/attendance/contentList/ContentList.tsx
+++ src/components/attendance/contentList/ContentList.tsx
@@ -1,25 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { StudentItem } from "../..";
+import MoveModal from "../moveModal/MoveModal";
 import * as S from "./style";
 
 const ContentList = () => {
+  const [selectCount, setSelectCount] = useState<number>(0);
+  const [moveModal, setMoveModal] = useState<boolean>(false);
+
   return (
-    <S.StudentList>
-      <ul className="sub-header">
-        <li>선택</li>
-        <li>학번</li>
-        <li>이름</li>
-        <li>8교시</li>
-        <li>9교시</li>
-        <li>10교시</li>
-      </ul>
-      <div className="student-list">
-        <StudentItem />
-        <StudentItem />
-        <StudentItem />
-        <StudentItem />
-      </div>
-    </S.StudentList>
+    <>
+      <MoveModal moveModal={moveModal} setMoveModal={setMoveModal} />
+      <S.StudentList>
+        <ul className="sub-header">
+          <li>선택</li>
+          <li>학번</li>
+          <li>이름</li>
+          <li>8교시</li>
+          <li>9교시</li>
+          <li>10교시</li>
+        </ul>
+        <div className="student-list">
+          <StudentItem
+            setMoveModal={setMoveModal}
+            setSelectCount={setSelectCount}
+            selectCount={selectCount}
+          />
+        </div>
+        <span className="count">{selectCount}명 선택됨</span>
+      </S.StudentList>
+    </>
   );
 };
 
