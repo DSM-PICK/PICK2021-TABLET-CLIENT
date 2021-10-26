583a747ebddffd445602b2b7defb62780819b083
강은빈 <kub0803@naver.com>
2021-10-26 21:17:24 +0900

    [ADD] 학생 리스트

--- src/components/attendance/contentList/ContentList.tsx
+++ src/components/attendance/contentList/ContentList.tsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React from "react";
+import { StudentItem } from "../..";
+import * as S from "./style";
 
 const ContentList = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <S.StudentList>
+      <ul className="sub-header">
+        <li>선택</li>
+        <li>학번</li>
+        <li>이름</li>
+        <li>8교시</li>
+        <li>9교시</li>
+        <li>10교시</li>
+      </ul>
+      <div className="student-list">
+        <StudentItem />
+        <StudentItem />
+        <StudentItem />
+        <StudentItem />
+      </div>
+    </S.StudentList>
+  );
 };
 
 export default ContentList;
