a17e42f48867c3a0d76333deeb36b95ddc0990a4
강은빈 <kub0803@naver.com>
2021-10-26 23:38:58 +0900

    [FEAT] 선택된 인원수 count

--- src/components/attendance/contentList/StdListItem.tsx
+++ src/components/attendance/contentList/StdListItem.tsx
@@ -2,22 +2,37 @@ import React, { useState } from "react";
 import StateContainer from "./StateContainer";
 import * as S from "./style";
 
-const StdListItem = () => {
+interface Props {
+  setSelectCount: any;
+  selectCount: number;
+  setMoveModal: any;
+}
+
+const StdListItem = ({ setSelectCount, selectCount, setMoveModal }: Props) => {
   const [check, setCheck] = useState<boolean>(false); //input check
 
   const std = ["8교시", "9교시", "10교시"];
 
   return (
-    <S.StudentItem check={check}>
-      <div className="checkbox">
-        <div onClick={() => setCheck(!check)} />
-      </div>
-      <span>2201</span>
-      <span>강은빈</span>
-      {std.map((_, index) => (
-        <StateContainer key={index} />
-      ))}
-    </S.StudentItem>
+    <>
+      <S.StudentItem check={check}>
+        <div className="checkbox">
+          <div
+            onClick={() => {
+              setCheck(!check);
+              check
+                ? setSelectCount(selectCount - 1)
+                : setSelectCount(selectCount + 1);
+            }}
+          />
+        </div>
+        <span>2201</span>
+        <span>강은빈</span>
+        {std.map((_, index) => (
+          <StateContainer key={index} setMoveModal={setMoveModal} />
+        ))}
+      </S.StudentItem>
+    </>
   );
 };
 
