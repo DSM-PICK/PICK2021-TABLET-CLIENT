1662cc5e287da7bb999b898398346292c258d278
강은빈 <kub0803@naver.com>
2021-10-26 21:16:50 +0900

    [FEAT] 학생 상태 기능

---
+++ src/components/attendance/contentList/StateContainer.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import StateList from "./StateListItem";
+import * as S from "./style";
+
+const StateContainer = () => {
+  const [stateSelect, setStateSelect] = useState<boolean>(false);
+  const [state, setState] = useState<string>("");
+
+  const list = ["출석", "이동", "외출", "무단", "현체", "귀가"];
+
+  return (
+    <S.StateWrapper state={state}>
+      <div className="state" onClick={() => setStateSelect(!stateSelect)}>
+        <span>{state}</span>
+      </div>
+
+      <S.StateSelectWrapper stateSelect={stateSelect}>
+        {list.map((item, index) => (
+          <StateList
+            key={index}
+            item={item}
+            setState={setState}
+            setStateSelect={setStateSelect}
+            stateSelect={stateSelect}
+          />
+        ))}
+      </S.StateSelectWrapper>
+    </S.StateWrapper>
+  );
+};
+
+export default StateContainer;
