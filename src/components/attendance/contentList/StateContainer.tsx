a0501be8f2b5101fdc0635005f2aefbefc98dcb1
강은빈 <kub0803@naver.com>
2021-10-26 23:38:24 +0900

    [MODIFY] 이동 교실 선택 모달 on/off

--- src/components/attendance/contentList/StateContainer.tsx
+++ src/components/attendance/contentList/StateContainer.tsx
@@ -1,12 +1,20 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import StateList from "./StateListItem";
 import * as S from "./style";
 
-const StateContainer = () => {
+interface Props {
+  setMoveModal: any;
+}
+
+const StateContainer = ({ setMoveModal }: Props) => {
   const [stateSelect, setStateSelect] = useState<boolean>(false);
   const [state, setState] = useState<string>("");
 
   const list = ["출석", "이동", "외출", "무단", "현체", "귀가"];
+
+  useEffect(() => {
+    state === "이동" ? setMoveModal(true) : setMoveModal(false);
+  }, [setMoveModal, state]);
 
   return (
     <S.StateWrapper state={state}>
