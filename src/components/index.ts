131cd2f58545c33f0c931aefc7e9b3111285e2b1
강은빈 <kub0803@naver.com>
2021-10-26 21:16:58 +0900

    [ADD] 컴포넌트 추가

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,6 @@
-export { default as Header } from "./header/Header";
-export { default as AttendanceHeader } from "./header/AttendanceHeader";
-export { default as Footer } from "./header/Footer";
+export { default as Header } from "./common/header/Header";
+export { default as AttendanceHeader } from "./common/header/AttendanceHeader";
+export { default as Footer } from "./common/header/Footer";
 export { default as MainPage } from "./mainPage/MainPage";
 export { default as Title } from "./mainPage/mainItem/Title";
 export { default as AttendanceSelect } from "./mainPage/mainItem/AttendanceSelect";
@@ -12,3 +12,5 @@ export { default as SubTitle } from "./mainPage/mainItem/items/SubTitle";
 export { default as Attendance } from "./attendance/Attendance";
 export { default as AttendanceClassBar } from "./attendance/attendanceBar/AttendanceClassBar";
 export { default as ContentHeader } from "./attendance/contentHeader/ContentHeader";
+export { default as ContentList } from "./attendance/contentList/ContentList";
+export { default as StudentItem } from "./attendance/contentList/StdListItem";
