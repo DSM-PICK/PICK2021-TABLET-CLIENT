87de0404c2a6ded0d84abbc03e0aa7cb19874873
강은빈 <kub0803@naver.com>
2021-10-26 21:16:08 +0900

    [ADD] 학생 리스트 UI

--- src/components/attendance/contentList/StdListItem.tsx
+++ src/components/attendance/contentList/StdListItem.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import StateContainer from "./StateContainer";
+import * as S from "./style";
 
 const StdListItem = () => {
-  return <div></div>;
+  const [check, setCheck] = useState<boolean>(false); //input check
+
+  const std = ["8교시", "9교시", "10교시"];
+
+  return (
+    <S.StudentItem check={check}>
+      <div className="checkbox">
+        <div onClick={() => setCheck(!check)} />
+      </div>
+      <span>2201</span>
+      <span>강은빈</span>
+      {std.map((_, index) => (
+        <StateContainer key={index} />
+      ))}
+    </S.StudentItem>
+  );
 };
 
 export default StdListItem;
