333d3c5f3c22e6915867a5394b6d58d36a64f100
강은빈 <kub0803@naver.com>
2021-10-27 23:23:27 +0900

    [ADD] 라우터 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
-import { Attendance, MainPage } from "./components";
+import { Attendance, MainPage, Calendar } from "./components";
 
 function App() {
   return (
     <BrowserRouter>
       <Route path="/" component={MainPage} exact />
       <Route path="/attendance" component={Attendance} exact />
+      <Route path="/calendar" component={Calendar} exact />
     </BrowserRouter>
   );
 }
