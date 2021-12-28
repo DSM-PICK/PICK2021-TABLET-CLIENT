c650ed77ca174ae30775e99ae77fa1a1c1b6515b
강은빈 <kub0803@naver.com>
2021-12-28 23:54:43 +0900

    [MODIFY] 일정교체 UI

--- src/components/calendar/calendarItem/CalendarChangeBtn.tsx
+++ src/components/calendar/calendarItem/CalendarChangeBtn.tsx
@@ -4,10 +4,10 @@ import * as S from "./style";
 const CalendarChangeBtn = () => {
   return (
     <S.ChangerBtnWrap>
-      <div className="item_center">
+      {/* <div className="item_center">
         일정 교체를 원하시면 일정 교체를 선택한 후
         <br /> 변경하실 일정을 선택해주세요.
-      </div>
+      </div> */}
       <button>일정 교체</button>
     </S.ChangerBtnWrap>
   );
