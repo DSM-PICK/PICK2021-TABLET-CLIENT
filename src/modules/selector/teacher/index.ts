e9588e641fce5f9c7973a804f5cb111a5d2cb151
강은빈 <kub0803@naver.com>
2021-12-28 23:14:49 +0900

    [ADD] selectorFamily 추가

--- src/modules/selector/teacher/index.ts
+++ src/modules/selector/teacher/index.ts
@@ -1,6 +1,7 @@
 import { selectorFamily } from "recoil";
-import { TeacherType } from "../../../lib/interface/teacher";
+import { StudentType, TeacherType } from "../../../lib/interface/teacher";
 import request from "../../../lib/api/axios";
+import teacher from "../../../lib/api/teacher";
 
 export const teacherInfoSelector = selectorFamily<TeacherType, string | null>({
   key: "teacherInfoSelector/get",
@@ -9,6 +10,18 @@ export const teacherInfoSelector = selectorFamily<TeacherType, string | null>({
 
     try {
       const res = await request(`/teacher/${id}/information`);
+      return res.data;
+    } catch (e) {
+      console.log(e);
+    }
+  },
+});
+
+export const studentNameSelector = selectorFamily<StudentType[], string>({
+  key: "studentNameSelector/get",
+  get: (name) => async () => {
+    try {
+      const res = await teacher.getStudentNameApi(name);
       return res.data;
     } catch (e) {
       console.log(e);
