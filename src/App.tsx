a49764749ad0662fa54adda2bc1ce1c64415745c
강은빈 <kub0803@naver.com>
2021-12-30 09:54:59 +0900

    [FEAT] toast feat add

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 import { Attendance, MainPage, Calendar, FirstPage, Login } from "./components";
+import { ToastContainer } from "react-toastify";
 
 function App() {
   return (
     <BrowserRouter>
+      <ToastContainer />
       <Route path="/" component={FirstPage} exact />
       <Route path="/login" component={Login} exact />
       <Route path="/home" component={MainPage} exact />
