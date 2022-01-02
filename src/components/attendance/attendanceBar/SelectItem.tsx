56c42ad30f0bc0c1b04a662d3fa63f5d0d07f32d
강은빈 <kub0803@naver.com>
2022-01-02 23:35:37 +0900

    [MODIFY] query modify

--- src/components/attendance/attendanceBar/SelectItem.tsx
+++ src/components/attendance/attendanceBar/SelectItem.tsx
@@ -1,25 +1,32 @@
 import React from "react";
 import { MainColor } from "../../../utils/color/color";
-import { FieldButtonType } from "../../../lib/interface/Attendance/FieldButtonType";
 import * as S from "./style";
+import { LocationType } from "../../../lib/interface/location";
+import { useHistory } from "react-router-dom";
+import { FieldButtonType } from "../../../lib/interface/Attendance/FieldButtonType";
 
 interface Props {
-  item: FieldButtonType;
+  item: LocationType | FieldButtonType;
   selected: number;
   selectedHandlerColor: any;
 }
 
 const SelectItem = ({ item, selected, selectedHandlerColor }: Props) => {
+  const { push } = useHistory();
+
   return (
     <S.SelectItem
-      onClick={() => selectedHandlerColor(item)}
+      onClick={() => {
+        selectedHandlerColor(item);
+        push(`/attendance?floor=${item?.floor}&id=${item.major_id}`);
+      }}
       style={
         selected === item.id
           ? { backgroundColor: `${MainColor}`, color: "white" }
           : { backgroundColor: "", color: "black" }
       }
     >
-      {item.field}
+      {item.name}
     </S.SelectItem>
   );
 };
